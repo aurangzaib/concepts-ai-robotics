fe67f790543291995935e0a23d5dc572e86a08f4
siddiqui <aurangzaibsiddiqui@gmail.com>
2017-04-18 16:17:35 +0200

    implemented twiddle algorithm — provides optimized parameter (tau_p, tau_d & tau_i) to pid controller.

--- pid-control.py
+++ pid-control.py
@@ -182,3 +182,141 @@ def pid_controller_race_track(_robot, _radius, _params, n=100, _speed=1.0):
     return _x_trajectory, _y_trajectory, _error / n
 
 
+def twiddle(_controller=pid_controller, _radius=0., _tolerance=0.2):
+    _number_of_params = 3
+    _p = [0. for row in range(_number_of_params)]
+    _dp = [1. for row in range(_number_of_params)]
+    """
+    if we make _dp[2] = 0 i.e. we turn off the integral then the results will have drift again
+    same goes for _dp[1]
+
+    making _dp[any_one] = 0 tells to remove p, i or d
+    that's why we initialize _dp with 1,1,1 always
+    """
+    _robot = make_robot()
+    _x, _y, _best_error = _controller(_robot, _radius, _p)
+    _iteration = 0
+    while sum(_dp) > _tolerance:
+        for _index in range(len(_p)):
+            _p[_index] += _dp[_index]
+            _robot = make_robot()
+            _x, _y, _error = _controller(_robot, _radius, _p)
+            """
+            if we get low error
+            then we will use it
+            """
+            if _error < _best_error:
+                _best_error = _error
+                _dp[_index] *= 1.1
+            else:
+                """
+                otherwise we will try other direction
+                (- _dp[_index]) will move to original position
+                (- 2*_dp[_index]) will move to oppositive direction
+                """
+                _p[_index] -= 2 * _dp[_index]
+                _robot = make_robot()
+                _x, _y, _error = _controller(_robot, _radius, _p)
+                if _error < _best_error:
+                    _best_error = _error
+                    _dp[_index] *= 1.1
+                else:
+                    """
+                    revert to the original
+                    """
+                    _p[_index] += _dp[_index]
+                    _dp[_index] *= 0.9
+        _iteration += 1
+    return _p
+
+
+# gives smooth path
+path_smooth = smooth_gradient_descent(path, 0.08, 0.1)
+# gives straight line connecting start and end points
+path_straight = smooth_gradient_descent(path, 0, 0.1)
+# no change
+path_like_original = smooth_gradient_descent(path, 0.5, 0)
+# cyclic paths with smoothing
+path_cyclic_smooth = smooth_constrained_cyclic(path_cyclic)
+
+number_of_steps = 200
+"""
+ - only proportional controller
+ - robot will pass through target axis but with high overshoot
+"""
+robot = make_robot(False)
+x_p, y_p = p_controller(robot,
+                        0.5,
+                        number_of_steps)
+"""
+ - proportional and differential (pd) controller
+ - robot will come to target axis with low overshoot
+"""
+robot = make_robot(False)
+x_pd, y_pd = pd_controller(robot,
+                           0.3,
+                           3.0,
+                           number_of_steps)
+"""
+ - pd + systematic bias (in this case steering drift)
+ - the cross track error (cte) will keep oscillating around initial value
+ - instead of coming to target axis, because of steering drift
+"""
+robot = make_robot(True)
+x_pd_drift, y_pd_drift = pd_controller(robot,
+                                       0.3,
+                                       3.0,
+                                       number_of_steps)
+"""
+ - to reduce systematic bias like steering drift
+ - we need to incorporate integral controller 
+ - which subtracts systematic cte
+ - and the value approaches to target without oscillating on the cte
+"""
+params = [0.3, 3.0, 0.01]
+robot = make_robot(True)
+x_pid, y_pid, err_pid = pid_controller(robot,
+                                       0,
+                                       params,
+                                       number_of_steps)
+"""
+twiddle --> Coordinate Ascent Method
+
+ - to further reduce the oscillation
+ - we first optimize the params and then use the pid_controller
+ - twiddle uses pid_controller inside it to optimize the parameter
+
+ - in this case, we actually use pid, but instead of assignment value to params ourselves
+ - we use twiddle to provide optimal params. after that we just use pid with these params.
+
+ - so twiddle is not replacement for pid, instead it provides optimized params to be used
+   for our specific pid controller
+"""
+params = twiddle()
+robot = make_robot(True)
+x_pid_twiddle, y_pid_twiddle, err_twiddle = pid_controller(robot,
+                                                           0,
+                                                           params,
+                                                           number_of_steps)
+
+robot = Robot()
+radius = 25.
+orientation = np.pi / 2
+robot.set(0.0, radius, orientation)
+params_race_track = twiddle(pid_controller_race_track, radius)
+# [10.0, 11.0, 0] --> can also be used instead of twiddle opitimzed params
+x_pid_race_track, y_pid_race_track, err_race_track = pid_controller_race_track(robot,
+                                                                               radius,
+                                                                               params_race_track,
+                                                                               number_of_steps)
+
+"""
+notice in the output that:
+    - p controller keeps oscillating around target
+    - pd controller reduces the oscillation around target
+    - systematic bias (steering drift) makes pd controller to oscillate around the initial y value
+    - pid controller removes the oscillation introduced by the systematic bias
+    - tau_i determines how fast pid removes the oscillations and approaches to the target point
+    - when pid is used with twiddle, it yields the best results and the oscillation is very low
+"""
+
