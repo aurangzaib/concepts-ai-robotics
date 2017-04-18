da53adc33bf7446da4e246bec81c79eac0976f41
siddiqui <aurangzaibsiddiqui@gmail.com>
2017-04-18 16:14:44 +0200

    implemented cyclic gradient-descent — path is in circulation — end-point=start-point

--- pid-control.py
+++ pid-control.py
@@ -44,3 +44,43 @@ def smooth_gradient_descent(_path, weight_data=0.0, weight_smooth=0.1, tolerance
     return _new_path
 
 
+def smooth_constrained_cyclic(_path, weight_data=0.07, weight_smooth=0.1, tolerance=0.000001):
+    weight_constrained_smooth = 0.5 * weight_smooth
+    _new_path = deepcopy(_path)
+    _change = tolerance
+    # keep iterating until change is smaller than the tolerance
+    while _change >= tolerance:
+        # set change to 0
+        _change = 0.
+        """
+        loop over each path on (i,j) and apply gradient descent equations.
+        each time update the difference and store in change variable.
+        """
+        for _i in range(len(_new_path)):
+            if not fix_points[_i]:
+                for _j in range(len(_new_path[0])):
+                    # values before updates
+                    _old_value = _new_path[_i][_j]
+
+                    # gradient and descent equations
+                    _gradient = weight_data * (_path[_i][_j] - _new_path[_i][_j])
+                    _descent = weight_smooth * (
+                        _new_path[(_i + 1) % len(_path)][_j] + _new_path[(_i - 1) % len(_path)][_j] - (
+                            2. * _new_path[_i][_j]))
+
+                    # constrained smoothing equations
+                    constrained_smooth_backward = 2. * (_new_path[(_i - 1) % len(_path)][_j]) - \
+                                                  _new_path[(_i - 2) % len(_path)][_j] - _new_path[_i][_j]
+                    constrained_smooth_forward = 2. * (_new_path[(_i + 1) % len(_path)][_j]) - \
+                                                 _new_path[(_i + 2) % len(_path)][_j] - _new_path[_i][_j]
+
+                    # save results of the equations
+                    _new_path[_i][_j] += _gradient + _descent
+                    _new_path[_i][_j] += weight_constrained_smooth * constrained_smooth_backward
+                    _new_path[_i][_j] += weight_constrained_smooth * constrained_smooth_forward
+
+                    # compare the outcomes of the gradient and descent results
+                    _change += abs(_new_path[_i][_j] - _old_value)
+    return _new_path
+
+
